07b88d6e39fda38452a72294fb85b4815f114900
Kabelo Moobi <giftmoobi@gmail.com>
2025-06-21 02:48:34 +0200

    refactor RootLayout to include pathname handling and BottomNav integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,37 @@
-import { Stack } from "expo-router";
+import { Stack, usePathname } from "expo-router";
+import React from "react";
+import { StatusBar, StyleSheet, View } from "react-native";
+import BottomNav from "./components/BottomNav";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const RootLayout = () => {
+  const pathname = usePathname();
+
+  const showBottomNav = [
+    "/(home)/index",
+    "/(shop)",
+    "/(shop)/categories",
+    "/(shop)/cart",
+    "/(shop)/wishlist",
+    "/(dashboard)/profile",
+  ].some((route) => pathname.startsWith(route));
+
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden={true} translucent={false} />
+      <Stack
+        initialRouteName="(onboarding)/onboarding"
+        screenOptions={{ headerShown: false }}
+      ></Stack>
+      <BottomNav />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+});
+
+export default RootLayout;
