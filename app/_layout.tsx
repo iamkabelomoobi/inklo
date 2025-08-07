0a006a8676e6f0da49dab6c80df7182ad961eca2
iamkabelomoobi <giftmoobi@gmail.com>
2025-08-07 12:21:12 +0200

    🎨 refactor RootLayout to remove pathname handling and comment out BottomNav

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,17 @@
-import { Stack, usePathname } from "expo-router";
+import { Stack } from "expo-router";
 import React from "react";
 import { StatusBar, StyleSheet, View } from "react-native";
 import BottomNav from "./components/BottomNav";
 
 const RootLayout = () => {
-  const pathname = usePathname();
-
-  const showBottomNav = [
-    "/(home)/index",
-    "/(shop)",
-    "/(shop)/categories",
-    "/(shop)/cart",
-    "/(shop)/wishlist",
-    "/(dashboard)/profile",
-  ].some((route) => pathname.startsWith(route));
-
   return (
     <View style={styles.container}>
       <StatusBar hidden={true} translucent={false} />
       <Stack
         initialRouteName="(onboarding)/onboarding"
-        screenOptions={{ headerShown: false }}
+        screenOptions={{ headerShown: false }} // set to false in production
       ></Stack>
-      <BottomNav />
+      {/* <BottomNav /> */}
     </View>
   );
 };
