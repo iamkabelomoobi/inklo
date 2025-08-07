9cf8f3fe8078a5dc39ed78bd0efbf9f478f2704a
iamkabelomoobi <giftmoobi@gmail.com>
2025-08-07 16:57:07 +0200

    🔥 refactor: simplify RootLayout by removing unused BottomNav and related logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,13 @@
-import { Stack, usePathname } from "expo-router";
-import React from "react";
+import { Stack } from "expo-router";
 import { StatusBar, StyleSheet, View } from "react-native";
-import BottomNav from "./components/BottomNav";
 
 const RootLayout = () => {
-  const pathname = usePathname();
-
-  // Hide BottomNav on (auth) and (onboarding) routes
-  const hideBottomNav =
-    pathname.startsWith("/(auth)") || pathname.startsWith("/(onboarding)");
-
   return (
     <View style={styles.container}>
-      <StatusBar hidden={false} translucent={false} />
-      <Stack
-        initialRouteName="(onboarding)/onboarding"
-        screenOptions={{ headerShown: false }}
-      >
+      <StatusBar hidden={true} translucent={false} />
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)" />
-        <Stack.Screen name="(home)/index" />
-        <Stack.Screen name="(shop)" />
       </Stack>
-      {/* {!hideBottomNav && <BottomNav />} */}
     </View>
   );
 };
