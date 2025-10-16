855a264856e0f68c6e23c79f37d20f858d7c3d58
iamkabelomoobi <giftmoobi@gmail.com>
2025-10-16 18:27:35 +0200

    🔥 Remove mobile and backend package, reset script, and TypeScript configuration files

--- mobile/assets/images/index.ts
+++
@@ -1,8 +0,0 @@
-export const Images = {
-  onboarding: {
-    step1: require("./onboarding/onboarding-1.jpg"),
-    step2: require("./onboarding/onboarding-2.jpg"),
-    step3: require("./onboarding/onboarding-3.jpg"),
-    step4: require("./onboarding/onboarding-4.jpg"),
-  },
-};
