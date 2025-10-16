6de40057f9c3ff238be806836e9d7d3a9cfffc70
iamkabelomoobi <giftmoobi@gmail.com>
2025-10-16 18:29:41 +0200

    ✨ Add new assets including fonts and images for onboarding screens

---
+++ assets/images/index.ts
@@ -0,0 +1,8 @@
+export const Images = {
+  onboarding: {
+    step1: require("./onboarding/onboarding-1.jpg"),
+    step2: require("./onboarding/onboarding-2.jpg"),
+    step3: require("./onboarding/onboarding-3.jpg"),
+    step4: require("./onboarding/onboarding-4.jpg"),
+  },
+};
